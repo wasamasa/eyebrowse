52563639a554bb9aa8a85cbd64975a310b5260ee
Vasilij Schneidermann <v.schneidermann@gmail.com>
2014-03-03 15:23:57 +0100

    Add another dependency
    `lexical-binding` is an Emacs 24 feature.

--- eyebrowse.el
+++ eyebrowse.el
@@ -5,7 +5,7 @@
 ;; Author: Vasilij Schneidermann <v.schneidermann@gmail.com>
 ;; URL: https://github.com/wasamasa/eyebrowse
 ;; Version: 0.1
-;; Package-Requires: ((dash "2.4.0") (s "1.4.0"))
+;; Package-Requires: ((dash "2.4.0") (s "1.4.0") (emacs "24"))
 ;; Keywords: convenience
 
 ;; This file is NOT part of GNU Emacs.
