1b3725ae7fa68589d795e385858bb34bba465342
Vasilij Schneidermann <v.schneidermann@gmail.com>
2015-04-28 19:37:31 +0200

    Bump version

--- eyebrowse.el
+++ eyebrowse.el
@@ -4,7 +4,7 @@
 
 ;; Author: Vasilij Schneidermann <v.schneidermann@gmail.com>
 ;; URL: https://github.com/wasamasa/eyebrowse
-;; Version: 0.4.0
+;; Version: 0.4.1
 ;; Package-Requires: ((dash "2.7.0"))
 ;; Keywords: convenience
 
