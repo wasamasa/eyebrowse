bc96c89afc8b00e8741350eb052c87f3779d7291
Vasilij Schneidermann <v.schneidermann@gmail.com>
2015-05-06 09:44:25 +0200

    Use bound-and-true-p for Evil

--- eyebrowse.el
+++ eyebrowse.el
@@ -432,7 +432,7 @@ is detected, it will bind gt, gT, gc and zx, too."
     (define-key map (kbd "C->") 'eyebrowse-next-window-config)
     (define-key map (kbd "C-'") 'eyebrowse-last-window-config)
     (define-key map (kbd "C-\"") 'eyebrowse-close-window-config)
-    (when (and (boundp 'evil-mode) evil-mode)
+    (when (bound-and-true-p evil-mode)
       (define-key evil-motion-state-map (kbd "gt")
         'eyebrowse-next-window-config)
       (define-key evil-motion-state-map (kbd "gT")
